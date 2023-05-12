fc6883c8591628a2e218015290e7a3564ef80b8f
Ruan Reis <ruancosme23@gmail.com>
2023-05-12 19:35:02 -0300

    finalizado divisão por categoria

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,18 @@ import Header from './components/Header';
 import Container from './components/Container';
 import Card from './components/Card';
 import videos from "./json/db.json"
+import Category from './components/Category';
+
+const categorias = [
+  "Animes",
+  "Programação",
+  "Esportes",
+  "Lazer"
+]
+
+function filtrarCategoria(id) {
+  return videos.filter( video => video.category === categorias[id])
+}
 
 function App() {
   return (
@@ -12,10 +24,23 @@ function App() {
       <Header/>
       <Banner imagem={"home"}/>
       <Container>
-        <h2>Anime</h2>
-        <section className='cards'>
-          { videos.map((video) => <Card id={video.id} key={video.id} /> )}
-        </section>
+
+        <Category category={'Animes'}>
+          {filtrarCategoria(0).map((video) => <Card id={video.id} key={video.id} />)} 
+        </Category>
+
+        <Category category={'Programação'}>
+          {filtrarCategoria(1).map((video) => <Card id={video.id} key={video.id} />)} 
+        </Category>
+
+        <Category category={'Esportes'}>
+          {filtrarCategoria(2).map((video) => <Card id={video.id} key={video.id} />)} 
+        </Category>
+
+        <Category category={'Lazer'}>
+          {filtrarCategoria(3).map((video) => <Card id={video.id} key={video.id} />)} 
+        </Category>
+
       </Container>
       <Footer/>
     </div>
