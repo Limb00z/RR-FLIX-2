350f0e3cdd14e391550455db3fdc6e8c977b7947
Ruan Reis <ruancosme23@gmail.com>
2023-05-13 11:41:47 -0300

    finalizado o HOME

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,31 @@
+import './App.css';
+import Footer from './components/Footer';
+import Banner from './components/Banner';
+import Header from './components/Header';
+import Container from './components/Container';
+import Card from './components/Card';
+import Category,{categorias, filtrarCategoria} from './components/Category';
+
+function Home() {
+  return (
+    <div>
+      <Header/>
+      <Banner imagem={"home"}/>
+      <Container>
+
+        {
+          categorias.map((category, index) =>
+            <Category category={category}>
+              {filtrarCategoria(index).map((video) =>
+                <Card id={video.id} key={video.id} /> )}
+            </Category>
+          )
+        }
+
+      </Container>
+      <Footer/>
+    </div>
+  );
+}
+
+export default Home;
