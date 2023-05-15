7e85fc3513514ec9791e4424ef27535a9ad92ccc
Ruan Reis <ruancosme23@gmail.com>
2023-05-15 11:07:22 -0300

    finalizado a parte o componente de busca. 2/3

--- src/components/BuscaVideos/index.jsx
+++ src/components/BuscaVideos/index.jsx
@@ -1,14 +1,29 @@
 import styles from "./BuscaVideos.module.css"
 import VideoList from "../../components/VideoList"
+import { useState } from "react"
+
+//Filtrar videos por categoria ou titulo
+function filtrarVideos(videos, buscaTexto) {
+    return videos.filter((video) => video.category.includes(buscaTexto) || video.title.includes(buscaTexto))
+}
 
 export default function BuscaVideos({videos}) {
+    
+    const [buscaTexto, setBuscaTexto] = useState("Medo, desistência ou qualquer coisa inútil")
+    
+    const achaVideos = filtrarVideos(videos, buscaTexto)
+    
     return (
         <section className={styles.container}>
             <input
                 type="search"
                 placeholder="Pesquisar..."
+                onChange={evento => setBuscaTexto(evento.target.value)}
             />
-            <VideoList videos={videos} />
+            <VideoList
+                videos={achaVideos}
+                emptyHeading={`Sem vídeos sobre "${buscaTexto}"`}
+            />
         </section>
     )
 }
--- src/pages/Busca/index.jsx
+++ src/pages/Busca/index.jsx
@@ -1,9 +1,9 @@
 import Container from "../../components/Container"
 import Footer from "../../components/Footer"
 import Header from "../../components/Header"
-import VideoList from "../../components/VideoList"
 import styles from "./Busca.module.css"
 import videos from "../../json/db.json"
+import BuscaVideos from "../../components/BuscaVideos"
 
 export default function Busca() {
     return (
@@ -12,7 +12,7 @@ export default function Busca() {
             <Container>
             <section className={styles.busca}>
                     <h2>Pesquisar </h2>
-                    <VideoList videos={videos} />
+                    <BuscaVideos videos={videos} />
             </section>
             </Container>
             <Footer/>
