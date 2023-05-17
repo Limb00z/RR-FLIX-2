81c4b500a70fd85fb09ab3f3d637a93b05b6dfe1
Ruan Reis <ruancosme23@gmail.com>
2023-05-17 18:40:43 -0300

    add icon fav

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom";
-import styles from "./Card.module.css"
+import styles from "./Card.module.css";
+import iconUnFavorite from "./unfavorite.png"
+import iconFavorite from "./favorite.png"
 
-export default function Card({id}) {
-    return (
-        <section className={styles.card}>
-            <Link to={`/assistir/${id}`}>
-                            
-                <img src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`} alt="Capa do vídeo" />
-                
-            </Link>
-        </section>
-    );
-};
+export default function Card({ id }) {
+  return (
+    <section className={styles.card}>
+      <Link to={`/assistir/${id}`}>
+        <img
+          src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`}
+                  alt="Capa do vídeo"
+                  className={styles.capa}
+        />
+      </Link>
+          <figure className={styles.icon}>
+              <img src={iconFavorite} alt="icone de coração, favorito" />
+        </figure>
+    </section>
+  );
+}
--- src/pages/Favoritos/index.jsx
+++ src/pages/Favoritos/index.jsx
@@ -1,19 +1,19 @@
 import styles from "./Favoritos.module.css";
-import Header from "../../components/Header"
-import Container from "../../components/Container"
-import Footer from "../../components/Footer"
+import Header from "../../components/Header";
+import Container from "../../components/Container";
+import Footer from "../../components/Footer";
 
 export default function Favoritos() {
-    return (
-        <>
-            <Header />
-            <Container>
-                <h2>Meus Favoritos</h2>
-                <section className={styles.favoritos}>
-                    Lista de Favoritos
-                </section>
-            </Container>
-            <Footer />
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <Container>
+        <section className={styles.favoritos}>
+          <h2>Meus Favoritos</h2>
+          Lista de Favoritos
+        </section>
+      </Container>
+      <Footer />
+    </>
+  );
 }
