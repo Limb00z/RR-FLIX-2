3bdb25c2f2314a4730e39f77c346f94c2e504d40
Ruan Reis <ruancosme23@gmail.com>
2023-05-18 19:57:50 -0300

    adicionando favoritos na página

--- src/pages/Favoritos/index.jsx
+++ src/pages/Favoritos/index.jsx
@@ -2,15 +2,20 @@ import styles from "./Favoritos.module.css";
 import Header from "../../components/Header";
 import Container from "../../components/Container";
 import Footer from "../../components/Footer";
+import VideoList from "../../components/VideoList";
+import { useFavoritoContext } from "../../contexts/Favoritos";
 
 export default function Favoritos() {
+  
+  const{ favorito} = useFavoritoContext()
+  
   return (
     <>
       <Header />
       <Container>
         <section className={styles.favoritos}>
           <h2>Meus Favoritos</h2>
-          Lista de Favoritos
+          {<VideoList videos={favorito} emptyHeading="Sem favoritos, por enquanto..."/>}
         </section>
       </Container>
       <Footer />
