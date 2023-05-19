8b96c2cdb270cef86ba1befc6591538094b56fb2
Ruan Reis <ruancosme23@gmail.com>
2023-05-19 19:45:53 -0300

    fazendo a transmissao dos dados

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,30 +1,47 @@
 import styles from "./Form.module.css"
 import { categorias} from "../Category"
+import { useState } from "react"
 
 export default function Form() {
+
+    const [url, setUrl] = useState('')
+    const [categoria, setCategoria] = useState('')
+
+    function onSave(e) {
+        e.preventDefault();
+        console.log(url,categoria);
+    }
+
     return (
         <section className={styles.container}>
             <h2>Cadastro de vídeo</h2>
-            <form >
+            <form onSubmit={onSave}>
                 <div>
                     <label>URL do Vídeo</label>
                     <input
                         type="text"
                         placeholder="Digite a URL do vídeo"
                         required="required"
+                        value={url}
+                        onChange={e=> setUrl(e.target.value)}
                     />
                 </div>
                 <div>
                     <label>Categoria</label>
-                    <select>
+                    <select
+                        required="required"
+                        value={categoria}
+                        onChange={e=> setCategoria(e.target.value)}
+                    >
                         <option value="-" selected>Selecione uma categoria</option>
                         {categorias.map(item => {
                             return <option value={item}>{item}</option>
                         })}
                     </select>
                 </div>
+                <button>Cadastrar</button>
             </form>
-            <button>Cadastrar</button>
+            
         </section>
     )
 }
