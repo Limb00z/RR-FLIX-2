113ced577760d9eb09a1969839d76160035ea50b
Ruan Reis <ruancosme23@gmail.com>
2023-05-19 18:24:45 -0300

    Adicionando a pagina em Routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,6 +4,7 @@ import Assistir from "./pages/Assistir";
 import PageNotFound from "./pages/PageNotFound";
 import Busca from "./pages/Busca";
 import Favoritos from "./pages/Favoritos";
+import CadastrarVideo from "./pages/CadastrarVideo";
 import FavoritosProvider from "./contexts/Favoritos";
 
 export default function AppRoutes() {
@@ -15,6 +16,7 @@ export default function AppRoutes() {
                     <Route path="/assistir/:id" element={<Assistir/>} />
                     <Route path="/busca" element={<Busca />} />
                     <Route path="/favoritos" element={<Favoritos />} />
+                    <Route path="/cadastrar" element={<CadastrarVideo />} />
                     <Route path="*" element={<PageNotFound/>} />
                 </Routes>
             </FavoritosProvider>
