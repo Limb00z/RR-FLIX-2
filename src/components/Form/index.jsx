aca1a86302b6a5c765650e4030ff31d4a7850f77
Ruan Reis <ruancosme23@gmail.com>
2023-05-20 19:31:45 -0300

    controle de dados da url e validação do Form

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -7,21 +7,52 @@ export default function Form() {
     const [url, setUrl] = useState('')
     const [categoria, setCategoria] = useState('')
     const [videos, setVideos] = useState([])
-    const [errors, setErrors]= useState('')
+    const [errors, setErrors] = useState('')
+    
+    //validar url do youtube 
+    function validarURl(url) {
+        const regex = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:embed\/|watch\?v=)|youtu\.be\/)([a-zA-Z0-9\-_]+)$/
+
+        if (!regex.test(url)) {
+            setErrors("ERRO: URL inválida");
+            return false
+        } else {
+            return url.substring(32, 43) // pegar o id do video 
+            
+        }
+    }
 
     function onSave(e) {
         e.preventDefault();
 
-        //guardar a url e a categoria
-        const newVideo = { url, categoria }
-        setVideos([...videos, newVideo])
+         //validar categoria 
+         if (!categoria || categoria === "-") {
+            setErrors('Selecione uma categoria');
+            return;
+        } else {
+            setErrors('')
+        }
 
-        localStorage.setItem('videos', JSON.stringify([...videos, newVideo]))
+        //validar url do youtube    
+        const urlVideo = validarURl(url)
+        if (urlVideo && categoria) {
+            
+            //guardar a url e a categoria
+            const newVideo = { url, categoria }
+            setVideos([...videos, newVideo])
 
-        //limpar o formulario
-        setUrl('')
-        setCategoria('')
+            localStorage.setItem('videos', JSON.stringify([...videos, newVideo]))
+
+            //limpar o formulario
+            setUrl('')
+            setCategoria('')
         
+        } else {
+            setErrors('ERRO: Por favor, preencha todos os campos corretamente')
+        }
+        
+       
+
     }
 
     return (
@@ -35,7 +66,9 @@ export default function Form() {
                         placeholder="Digite a URL do vídeo"
                         required="required"
                         value={url}
-                        onChange={e=> setUrl(e.target.value)}
+                        onChange={e => setUrl(e.target.value)}
+                        maxLength="43"
+                        minLength="43"
                     />
                 </div>
                 <div>
@@ -52,6 +85,9 @@ export default function Form() {
                     </select>
                 </div>
                 <button>Cadastrar</button>
+                <div>
+                    {errors}
+                </div>
             </form>
             
         </section>
